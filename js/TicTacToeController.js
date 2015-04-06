2fdd74e05a2d0e830cc2704fc00e2c36675869ae
wesr7 <jwesleyrodgers@gmail.com>
2015-04-06 07:57:47 -0700

    game works locally. deployed to firebase but not saving firebase object.

--- js/TicTacToeController.js
+++ js/TicTacToeController.js
@@ -1,64 +1,110 @@
 (function(){
 
-angular
-	.module("TicApp").controller("TicTacToeController", TicTacToeController);
+	angular
+	.module("TicApp")
+	.controller("TicTacToeController", TicTacToeController);
 
-	function TicTacToeController(){
+	TicTacToeController.$inject = ['$firebaseArray', '$firebaseObject'];
 
- 		var self=this;
- 		//this is my gameboard
- 		self.board = [
- 		{
- 			box: "1",
- 			piece: null
- 		}, {
- 			box: "2",
- 			piece: null
- 		}, {
- 			box: "3",
- 			piece: null
- 		}, {
- 			box: "4",
- 			piece: null
- 		}, {
- 			box: "5",
- 			piece: null
- 		}, {
- 			box: "6",
- 			piece: null
- 		}, {
- 			box: "7",
- 			piece: null
- 		}, {
- 			box: "8",
- 			piece: null
- 		}, {
- 			box: "9",
- 			piece: null
- 		}];
+	function TicTacToeController ($firebaseArray, $firebaseObject) {
 
- 		//two players
-		self.turn = 0;
-		self.isDisabled = false;
-		self.winCombos = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"], ["1", "4", "7"], ["2", "5", "8"], ["3", "6", "9"], ["1", "5", "9"], ["3", "5", "9"]];
-		//
-		self.setSquare = function ($index){
-			self.board[$index].piece = self.turn;
-			self.toggle();
-			};
-		self.toggle = function () {
-			if(self.turn===0) {
-				self.turn++ ;
-			} else if (self.turn===1) {
-				self.turn--;
-			}
+		var self = this;
+                        self.turn = 1;
+                        self.turnCounter = 0;
+                        self.player1 = player1;
+                        self.player2 = player2;
+                        self.game = tttGame();
+                        self.getMove = getMove;
+                        self.gameWinner = gameWinner;
+		self.resetGame = resetGame;
+ 		self.grid = [
+ 		{box: ""}, {box: ""}, {box: ""}, {box: ""}, {box: ""}, {box: ""}, {box: ""}, {box: ""}, {box: ""}];
+                          console.log(self.grid);
 
-		};
+            function tttGame() {
+
+                        var ref = new Firebase("https://westttapp.firebaseio.com/");
+                        var game = $firebaseObject(ref);
+                        return game;
+                        }
+                        self.game.$save();
+
+            function player1() {
+
+                        self.game.player1 = player1;
+                        self.game.$save();
+            }
+            function player2() {
+
+                        self.game.player2 = player2;
+                        self.game.$save();
+            }
+
+            function getMove (index) {
+                self.turnCounter++;
+
+                        if (self.grid[index].box){
+                                    return false;
+                        } if (self.turn===1){
+                        self.grid[index].box = "X";
+                                    self.turn++;
+                        self.game.$save();
+                        } else {
+                        self.grid[index].box = "O";
+                                    self.turn--;
+                        self.game.$save();
+                        }
+                        gameWinner(self.grid[index].box);
+                        self.game.$save();
+            }
+
+            function gameWinner(item) {
+                        console.log(item);
+
+                        if((self.grid[0].box === item && self.grid[1].box === item && self.grid[2].box === item) ||
+
+                            (self.grid[3].box === item && self.grid[4].box === item && self.grid[5].box === item) ||
+
+                            (self.grid[6].box === item && self.grid[7].box === item && self.grid[8].box === item) ||
+
+                            (self.grid[0].box === item && self.grid[3].box === item && self.grid[6].box === item) ||
+
+                            (self.grid[1].box === item && self.grid[4].box === item && self.grid[7].box === item) ||
+
+                            (self.grid[2].box === item && self.grid[5].box === item && self.grid[8].box === item) ||
+
+                            (self.grid[0].box === item && self.grid[4].box === item && self.grid[8].box === item) ||
+
+                            (self.grid[2].box === item && self.grid[4].box === item && self.grid[6].box === item)
+                            ){
+                                self.showWinner = item + " is the WINNER!!!!";
+                                console.log(item + " win");
+                            }
+                                else {
+                            if(
+                                self.turnCounter === 9
+                                )
+                            self.showCats = "CATS GAME!!!";
+
+                            }
+                        }
+
+
+            function resetGame() {
+                        self.turn = 1;
+                        self.turnCounter = 9;
+                        self.showWinner = "";
+                        self.showCats = "";
+
+                        for (var i = 0; i < self.grid.length; i++) {
+                            self.grid[i].box = "";
+                            console.log(self.grid[i].box);
+                        }
+            }
 	}
-
-
-
 })();
+
+
 
 
 
--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 (function(){
 
-angular.module("TicApp", []);
+angular
+        .module("TicApp", ['firebase']);
 
 
 })();
